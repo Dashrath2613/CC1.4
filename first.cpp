96d4d02db9700bf94e365287191cefa97ca9907c
Dashrath2613 <138874170+Dashrath2613@users.noreply.github.com>
2024-02-22 00:12:13 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int a ,b,c;
+cout<<"Dashrath Nandan ------> 21BCS11716"<<endl;
+cout<<"Enter the two numbers to add"<<endl;
+cin>>a>>b;
+c=a+b;
+cout<<"The sum of"<<a<<"and"<<b<<"is"<<c<<endl;
+return 0;
+}
